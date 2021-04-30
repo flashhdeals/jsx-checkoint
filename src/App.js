5b7ff54b4299b5a1a6df0f1810fa87214b4ff73e
abdulla <abdulla@flashh.co.uk>
2021-04-30 08:19:20 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
-import './App.css';
-
+import './style.css';
+import ImageInSrc from './imageInSrc.jpg';
+import myVideo from './myVideo.mp4';
 function App() {
+  const css = {
+    border:"solid 1px black",
+    maxWidth:'100vw'
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={css}>
+    <h1 className="title red">Abdulla</h1>
+    <br />
+    <img src={ImageInSrc} className="responsiveImg"/>
+    <br />
+    <img src="/imageInPublic.jpg" className="responsiveImg"/>
+    <br />
+    <video width="320" height="240" controls>
+      <source src={myVideo} type="video/mp4" />
+    </video>    
     </div>
   );
 }
